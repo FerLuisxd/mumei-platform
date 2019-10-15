0484ae77e2f7d5b6c0063955fcde5dab56a5d180
Luis Vilca <ferluisxd@gmail.com>
2019-10-15 09:35:28 -0500

    still trying to get mircrophone up and running

--- Code/src/renderer/pages/index.vue
+++ Code/src/renderer/pages/index.vue
@@ -19,8 +19,7 @@
         Electron.js
       </div>
     </div>
-  <v-btn>dsadssadsadas</v-btn>
-<audio id="player" controls></audio>
+  <v-btn>dsadssadsadsas</v-btn>
   </div>
 </template>
 
@@ -34,7 +33,10 @@ export default {
   },
   data () {
     return {
-      externalContent: ''
+      externalContent: '',
+      audio_stream: null,
+      audio_context: null,
+      recorder: null
     }
   },
   methods: {
@@ -42,22 +44,110 @@ export default {
       remote.shell.openExternal(url)
     },
     startAudio(){
+    },
+    speech(){
+          window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+    let finalTranscript = '';
+    let recognition = new window.SpeechRecognition();
+    recognition.interimResults = true;
+    recognition.maxAlternatives = 10;
+    recognition.continuous = true;
+    recognition.onresult = (event) => {
+      console.log("event?")
+      let interimTranscript = '';
+      for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+        let transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) {
+          finalTranscript += transcript;
+        } else {
+          interimTranscript += transcript;
+        }
+      }
+      console.log(interimTranscript)
+      document.querySelector('body').innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</>';
+    }
+    recognition.start();
+    },
+    handleClickStart(){
+        let shouldStop = false;
+  let stopped = false;
+  // const downloadLink = document.getElementById('download');
+  // const stopButton = document.getElementById('stop');
+
+  // stopButton.addEventListener('click', function() {
+  //   shouldStop = true;
+  // })
+ var handleSuccess = function(stream) {  
+    const options = {mimeType: 'video/webm;codecs=vp9'};
+    const recordedChunks = [];
+    const mediaRecorder = new MediaRecorder(stream, options);  
+    console.log("dat1")
+    console.log(mediaRecorder)
+    mediaRecorder.addEventListener('dataavailable', function(e) {
+          console.log("dat2")
+      if (e.data.size > 0) {
+        recordedChunks.push(e.data);
+        console.log("data")
+      }
+
+      if(shouldStop === true && stopped === false) {
+        console.log("pare")
+        mediaRecorder.stop();
+        stopped = true;
+      }
+    });
+
+    mediaRecorder.addEventListener('stop', function() {
+      console.log("pare")
+      // downloadLink.href = URL.createObjectURL(new Blob(recordedChunks));
+      // downloadLink.download = 'acetest.wav';
+    });
+
+    mediaRecorder.start();
+  };
+      navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+      .then(handleSuccess);
+      // navigator.mediaDevices.getUserMedia({ audio: true }).then((mediaStreamObject)=>{
+      //     this.audio_stream = mediaStreamObject
+      //     const input = this.audio_context.createMediaStreamSource(mediaStreamObject)
+      //     console.log("listening,", input)
+      // })
     }
   },
   async mounted(){
-  var player = document.getElementById('player');
-
-  var handleSuccess = function(stream) {
-    console.log(stream)
-    if (window.URL) {
-      player.src = window.URL.createObjectURL(stream);
-    } else {
-      player.src = stream;
+          window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+    let finalTranscript = '';
+    let recognition = new window.SpeechRecognition();
+    recognition.interimResults = true;
+    recognition.maxAlternatives = 10;
+    recognition.continuous = true;
+    
+    recognition.onresult = (event) => {
+      console.log("event?")
+      let interimTranscript = '';
+      for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+        let transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) {
+          finalTranscript += transcript;
+        } else {
+          interimTranscript += transcript;
+        }
+      }
+      console.log(interimTranscript)
+      document.querySelector('body').innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</>';
     }
-  };
-
-  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-      .then(handleSuccess)
+    recognition.start();
+   try {
+        window.AudioContext = window.AudioContext || window.webkitAudioContext
+        window.URL = window.URL || window.webkitURL
+        this.audio_context = new AudioContext()
+        console.log("this", this.audio_context)
+        // this.handleClickStart()
+        this.speech()
+      } catch (e) {
+        alert('No web audio support in this browser!')
+      }
+  
   }
 }
 </script>
